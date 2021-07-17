d93c85d01063cbef29229e6a7ffb9c3cb864d420
Mojtaba Safdari <sam.dreamkiller2018@gmail.com>
2021-07-17 19:13:58 +0430

    Add users comment section

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,3 +4,20 @@ var swiper = new Swiper(".audience-Swiper", {
     clickable: true,
   }
 });
+
+var swiper = new Swiper(".members .audience-Swiper", {
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints:{
+    1280:{
+      spaceBetween: 60,
+    }
+  }
+});
