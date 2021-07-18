f8539579042a8f307262f54a5636d1549968993e
Mojtaba Safdari <sam.dreamkiller2018@gmail.com>
2021-07-18 22:24:16 +0430

    Add footer section

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,10 @@
+// Swiper Slider
+
 var swiper = new Swiper(".audience-Swiper", {
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
-  }
+  },
 });
 
 var swiper = new Swiper(".members .audience-Swiper", {
@@ -15,9 +17,22 @@ var swiper = new Swiper(".members .audience-Swiper", {
     el: ".swiper-pagination",
     clickable: true,
   },
-  breakpoints:{
-    1280:{
+  breakpoints: {
+    1280: {
       spaceBetween: 60,
-    }
-  }
+    },
+  },
+});
+
+// Footer collapsble list
+
+const titles = document.querySelectorAll("footer .list-container .title");
+
+titles.forEach((item) => {
+  item.addEventListener("click", function () {
+    titles.forEach((item) => {
+      item.classList.remove("active");
+    });
+    this.classList.toggle("active");
+  });
 });
